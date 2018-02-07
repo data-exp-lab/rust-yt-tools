19a037bffb4f0b6a0b8e2b33298f165acd8abe68
Madicken Munk <madicken.munk@gmail.com>
2018-02-07 10:42:56 -0600

    printing pixel number from user input as string

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,35 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use yt_tools::{DataPixel, FixedResolutionBuffer};
 use stdweb::unstable::TryInto;
 use stdweb::web::{
+    IEventTarget,
     INode,
     HtmlElement,
     document,
+    WebSocket,
 };
 
-// use stdweb::unstable::TryInto;
-//use stdweb::web::{
-//    HTMLElement,
-//    document,
-//};
+use stdweb::web::event::{
+    IEvent,
+    IKeyboardEvent,
+    IMessageEvent,
+    KeypressEvent,
+    SocketOpenEvent,
+    SocketCloseEvent,
+    SocketErrorEvent,
+    SocketMessageEvent,
+};
+
+use stdweb::web::html_element::InputElement;
 
 
-//use std::io::BufReader;
+macro_rules! enclose {
+    ( ($( $x:ident ),*) $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
+}
 
 fn main() {
     stdweb::initialize();
@@ -88,9 +104,46 @@ fn main() {
         pix_count += 1;
     }
 
+    output_msg("DataPixel has been filled...");
+    let ws = WebSocket::new("wss://echo.websocket.org").unwrap();
+
+    ws.add_event_listener( enclose!( (output_msg) move |_: SocketOpenEvent| {
+        output_msg(&format!("> Enter a pixel value between 0 and {:?}", pix_count));
+    }));
+
+    ws.add_event_listener( enclose!( (output_msg) move |_: SocketErrorEvent| {
+        output_msg("> Connection Errored");
+    }));
+
+    ws.add_event_listener( enclose!( (output_msg) move |event: SocketCloseEvent| {
+        output_msg(&format!("> Connection Closed: {}", event.reason()));
+    }));
+
+    ws.add_event_listener( enclose!( (output_msg) move |event: SocketMessageEvent| {
+        // let pix_str = &event.data().into_text();
+        // let pix_int: u8 = pix_str.parse::<u8>().unwrap();
+        // output_msg(&format!("The value you chose was {:?}", pix_int));
+        output_msg(&format!("The value you chose was {:?}", &event.data().into_text().unwrap()));
+    }));
+
+    let text_entry: InputElement = document().query_selector( ".form input" ).unwrap().try_into().unwrap();
+    text_entry.add_event_listener( enclose!( (text_entry) move |event: KeypressEvent| {
+        if event.key() == "Enter" {
+            event.prevent_default();
+
+            let text: String = text_entry.value().try_into().unwrap();
+            if text.is_empty() == false {
+                text_entry.set_value("");
+                ws.send_text(&text);
+            }
+        }
+    }));
+
     let frb = FixedResolutionBuffer::new(1024, 1024, (0.0, 1.0), (0.0, 1.0));
 
     println!("Index 0, 0 becomes {}; Index 512, 512 becomes {}\n",
              frb.index(0, 0), frb.index(512, 512));
 
+    stdweb::event_loop();
+    
 }
