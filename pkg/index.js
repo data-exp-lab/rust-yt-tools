8eac5fda2974ec3c2ecc4fecfbd6af35150ecadb
Matthew Turk <matthewturk@gmail.com>
2018-04-26 08:32:53 -0500

    Adding normalizers and exporting them

--- pkg/index.js
+++ pkg/index.js
@@ -1,6 +1,6 @@
-import { FixedResolutionBuffer, VariableMesh } from './yt_tools';
+import { FixedResolutionBuffer, VariableMesh, Colormaps } from './yt_tools';
 import { booted } from './yt_tools_bg';
 
 booted.then(() => console.log("Hello World!"));
 
-export { FixedResolutionBuffer, VariableMesh, booted };
+export { FixedResolutionBuffer, VariableMesh, Colormaps, booted };
--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,24 @@ pub struct VariableMesh {
 }
 
 #[wasm_bindgen]
+impl VariableMesh {
+  pub fn new(px: Vec<f64>, py: Vec<f64>, pdx: Vec<f64>, pdy: Vec<f64>, val: Vec<f64>) -> VariableMesh {
+      VariableMesh {
+          px, py, pdx, pdy, val
+      }
+  }
+}
+
+pub fn get_normalizer(name: String) -> (fn(f64) -> f64) {
+    let f: fn(f64) -> f64 = match(name.to_lowercase().as_ref()) {
+        "log" => |f| f.log10(),
+        "linear" => |f| f,
+        _ => |f| f,
+    };
+    f
+}
+
+#[wasm_bindgen]
 pub struct Colormaps {
     // These colormaps are stored unrolled, such that they are RGBA RGBA RGBA
   color_maps: HashMap<String, Vec<u8>>,
@@ -41,14 +59,16 @@ pub struct Colormaps {
 impl Colormaps {
   pub fn new() -> Colormaps {
     let mut color_maps = HashMap::new();
-    let mut default_cmap: Vec<u8> = Vec::with_capacity(4 * 255);
-    for i in 0..255 {
-        default_cmap.push(i);
-        default_cmap.push(i);
-        default_cmap.push(i);
-        default_cmap.push(255);
+    let mut default_cmap: Vec<u8> = Vec::with_capacity(4 * 256);
+    let mut i: usize = 0;
+    default_cmap.resize(4*256, 0);
+    for i in 0..256 {
+        default_cmap[i * 4 + 0] = i as u8;
+        default_cmap[i * 4 + 1] = i as u8;
+        default_cmap[i * 4 + 2] = i as u8;
+        default_cmap[i * 4 + 3] = 255;
     }
-    color_maps.insert(String::from("default"), default_cmap);
+    color_maps.insert(String::from("default"), default_cmap.clone());
     Colormaps {
         color_maps,
     }
@@ -58,58 +78,78 @@ impl Colormaps {
     self.color_maps.insert(name, table);
   }
 
-  pub fn normalize_min(&mut self, name: String, buffer: Vec<f64>, min_val: f64) -> Vec<u8> {
-    self.normalize(name, buffer, Some(min_val), None)
+  // Once we have Option support in wasm-bindgen we'll be able to get rid of these.
+  pub fn normalize(&mut self, name: String, buffer: Vec<f64>, take_log: bool) -> Vec<u8> {
+    self.normalize_(name, buffer, None, None, take_log)
   }
 
-  pub fn normalize_max(&mut self, name: String, buffer: Vec<f64>, max_val: f64) -> Vec<u8> {
-    self.normalize(name, buffer, None, Some(max_val))
+  pub fn normalize_min(&mut self, name: String, buffer: Vec<f64>, min_val: f64, take_log: bool) -> Vec<u8> {
+    self.normalize_(name, buffer, Some(min_val), None, take_log)
   }
 
-  pub fn normalize_min_max(&mut self, name: String, buffer: Vec<f64>, min_val: f64, max_val: f64) -> Vec<u8> {
-    self.normalize(name, buffer, Some(min_val), Some(max_val))
+  pub fn normalize_max(&mut self, name: String, buffer: Vec<f64>, max_val: f64, take_log: bool) -> Vec<u8> {
+    self.normalize_(name, buffer, None, Some(max_val), take_log)
+  }
+
+  pub fn normalize_min_max(&mut self, name: String, buffer: Vec<f64>, min_val: f64, max_val: f64, take_log: bool) -> Vec<u8> {
+    self.normalize_(name, buffer, Some(min_val), Some(max_val), take_log)
   }
 }
 
 // Note that this is a separate impl block, so we do not have the wasm code generated for it; as of
 // the time of writing, Option did not work.
 impl Colormaps {
-  pub fn normalize(&mut self, name: String, buffer: Vec<f64>, min_val: Option<f64>, max_val: Option<f64>) -> Vec<u8> {
-    let cmin_val: f64 = 0.0;
-    let cmax_val: f64 = 0.0;
+  pub fn normalize_(&mut self, name: String, buffer: Vec<f64>, min_val: Option<f64>, max_val: Option<f64>,
+                   take_log: bool) -> Vec<u8> {
+    let f = match(take_log) {
+        true => get_normalizer("log".to_string()),
+        false => get_normalizer("linear".to_string()),
+    };
+    let mut cmin_val: f64 = 0.0;
+    let mut cmax_val: f64 = 0.0;
     if min_val == None || max_val == None {
-      let cmin_val = f64::MAX;
-      let cmax_val = f64::MIN;
+        log_f64("We're looking for something ...", 0.0);
+      cmin_val = f64::MAX;
+      cmax_val = f64::MIN;
       for i in 0..buffer.len() {
-          let cmin_val = cmax_val.min(buffer[i]);
-          let cmax_val = cmax_val.max(buffer[i]);
+          cmin_val = cmin_val.min(buffer[i]);
+          cmax_val = cmax_val.max(buffer[i]);
       }
+      log_f64("Found ", cmin_val);
+      log_f64("Found ", cmax_val);
     }
-    let cmin_val = match(min_val) {
+    cmin_val = match(min_val) {
         Some(v) => v,
         None => cmin_val,
     };
-    let cmax_val = match(max_val) {
+    cmax_val = match(max_val) {
         Some(v) => v,
         None => cmax_val,
     };
     let mut image: Vec<u8> = Vec::with_capacity(buffer.len() * 4);
     if !self.color_maps.contains_key(&name) {
         let name = "default";
+        log_f64("Choosing default colormap", 1.0);
     }
     let cmap = match self.color_maps.get(&name) {
         Some(cmap) => cmap,
         None => panic!("Colormap {:?} does not exist.", name)
     };
+    log_f64("cmin_val", cmin_val);
+    log_f64("cmax_val", cmax_val);
+    cmin_val = f(cmin_val);
+    cmax_val = f(cmax_val);
+    log_f64("cmin_val", cmin_val);
+    log_f64("cmax_val", cmax_val);
     image.resize(buffer.len() * 4, 0);
     for i in 0..buffer.len() {
-        let ind = i * 4;
-        let scaled = (buffer[i].log10() - cmin_val)/(cmax_val - cmin_val);
-        let bin_id = (scaled * 255.0).floor() as usize;
-        image[ind + 0] = cmap[bin_id * 4 + 0];
-        image[ind + 1] = cmap[bin_id * 4 + 1];
-        image[ind + 2] = cmap[bin_id * 4 + 2];
-        image[ind + 3] = cmap[bin_id * 4 + 3];
+        let scaled = ((f(buffer[i]) - cmin_val)/(cmax_val - cmin_val))
+                     .min(1.0).max(0.0);
+        let bin_id = (scaled * 255.0) as usize;
+        image[i*4 + 0] = cmap[bin_id * 4 + 0];
+        image[i*4 + 1] = cmap[bin_id * 4 + 1];
+        image[i*4 + 2] = cmap[bin_id * 4 + 2];
+        image[i*4 + 3] = cmap[bin_id * 4 + 3];
     }
     image
   }
@@ -140,6 +180,11 @@ impl FixedResolutionBuffer {
   }
 
   #[wasm_bindgen]
+  pub fn get_buffer(&mut self) -> Vec<f64> {
+    self.buffer.clone()
+  }
+
+  #[wasm_bindgen]
   pub fn dump_image(&mut self) -> Vec<u8> {
       let mi = f64::MAX;
       let ma = f64::MIN;
@@ -167,17 +212,18 @@ impl FixedResolutionBuffer {
   }
 
   #[wasm_bindgen]
-  pub fn deposit(&mut self, vmesh: &VariableMesh, pix_i: usize) {
+  pub fn deposit(&mut self, vmesh: &VariableMesh) -> u32 {
+    let mut count : u32 = 0;
     for pix_i in 0..vmesh.px.len() {
         // Compute our left edge pixel
         if vmesh.px[pix_i] + vmesh.pdx[pix_i] < self.x_low {
-            return;
+            continue;
         } else if vmesh.py[pix_i] + vmesh.pdy[pix_i] < self.y_low {
-            return;
+            continue;
         } else if vmesh.px[pix_i] - vmesh.pdx[pix_i] > self.x_high {
-            return;
+            continue;
         } else if vmesh.py[pix_i] - vmesh.pdy[pix_i] > self.y_high {
-            return;
+            continue;
         }
         let lc: usize = (((vmesh.px[pix_i] - vmesh.pdx[pix_i] - self.x_low) * self.ipdx - 1.0)
                         .floor() as usize);
@@ -191,8 +237,18 @@ impl FixedResolutionBuffer {
         for i in lc.max(0)..rc.min(self.width) {
             for j in lr.max(0)..rr.min(self.height) {
                 self.buffer[i * self.width + j] = vmesh.val[pix_i];
+                count = count + 1;
             }
         }
     }
+    count
   }
+}
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_f64(s: &str, a: f64);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(s: &str, a: u32);
 }
