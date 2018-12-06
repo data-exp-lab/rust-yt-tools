5141710ea54c1caa5761d887ef7176092a0cac5a
Matthew Turk <matthewturk@gmail.com>
2018-12-06 16:12:39 -0600

    Add in stuff from the awesome wasm-pack-template

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
+extern crate cfg_if;
 extern crate wasm_bindgen;
-use wasm_bindgen::prelude::*;
 
+mod utils;
 mod colormaps;
 mod variable_mesh;
 mod fixed_resolution_buffer;
+
+use cfg_if::cfg_if;
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
